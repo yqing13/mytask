9ccb55f36ffd420e6108017f2ac33ff867e28a46
yqing13 <62823847+yqing13@users.noreply.github.com>
2021-05-29 13:52:50 +0800

    Create zqkk.js

---
+++ zqkk.js
@@ -0,0 +1,4 @@
+cron "8/30 * 1-6 * * *" script-path=https://raw.githubusercontent.com/ztxtop/x/main/zqread.js, enabled=true, tag=中青阅读
+
+cron "15/30 * * * *" script-path=https://raw.githubusercontent.com/ztxtop/x/main/youth.js, enabled=true, tag=中青签到、宝箱
+cron "30 6 * * *" script-path=https://raw.githubusercontent.com/ztxtop/x/main/youth_banner.js, enabled=true, tag=中青看看赚
